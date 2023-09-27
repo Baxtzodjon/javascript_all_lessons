ae55d3a5e322b5b937b992f58fe5b57eb181e04f
Baxtzodjon <rasulovbaxtzod@gmailcom>
2023-09-27 22:46:27 +0500

    js all lessons

---
+++ script.js
@@ -0,0 +1,80 @@
+// task number one 
+
+// let priceList = [30000, 2000, 11000, 33000, 2000, 100000,57000, 14000, 18000, 8000]
+
+// let from = prompt('цена от')
+// let up = prompt('цена до')
+
+// let findedCars = priceList.filter(car_price => car_price >= from && car_price <= up)
+
+// console.log(findedCars);
+
+
+// task number two
+
+// let students = ['baxtzod', 'islom', 'abdumanon', 'xojik', 'aziz'];
+// let longest_name = '' // 0
+
+// students.filter(item => {
+//     if (item.length > longest_name.length) {
+//         longest_name = item
+//     }
+// })
+
+// console.log(longest_name);
+
+
+// task number three
+
+// let arr = [6, 2, 6, 6, 2, 5, 6, 2, 4, 6, 78, 8, 2, 23]
+// let numbers = '' // '' всегда ровно 0
+
+// arr.filter(item => {
+//     if(item > numbers) {
+//         numbers = item
+//     }
+// })
+
+// console.log(numbers);
+
+
+// task number four
+
+
+// let arr = [6, 2, 6, 6, 2, 5, 6, 22, 4]
+// let students = ['baxtzod', 'islom', 'Bobur', 'abdumanon', 'xojik', 'aziz', 'Далер', 'Алишер'];
+// let sorted = students.sort((a, b) => a.length - b.length)
+
+
+// let nums = [1, 2, 3, 4]
+
+// let total = 0
+
+// nums.filter(num => total += num)
+
+// console.log(total);
+
+
+// task number five
+
+// Homework
+
+// let arr = [1, 2, false, 'hello', 24, 'world', undefined, null, 'error', 22];
+
+// let str = 0;
+// let nums = 0;
+// let others = 0;
+
+// for (let element of arr) {
+//     if (typeof element === 'string') {
+//         str++;
+//     } else if (typeof element === 'number') {
+//         nums++;
+//     } else {
+//         others++;
+//     }
+// }
+
+// console.log(`Strings element: ${str}`);
+// console.log(`Numbers element: ${nums}`);
+// console.log(`Others element: ${others}`);
